8eb7dbca1dee09f80b3daf001827bb68e9588374
Jamimofasa <james_morand@hotmail.com>
2020-10-01 13:40:14 -0700

    first commit

---
+++ Homework_4/Form1.Designer.vb
@@ -0,0 +1,298 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.txtPrice = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.lstItems = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtQuantity = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtSubTotal = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtTax = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.btnClearOrder = New System.Windows.Forms.Button()
+        Me.btnCheckOut = New System.Windows.Forms.Button()
+        Me.pic = New System.Windows.Forms.PictureBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtItem = New System.Windows.Forms.TextBox()
+        Me.btnUpdate = New System.Windows.Forms.Button()
+        Me.picLogo = New System.Windows.Forms.PictureBox()
+        CType(Me.pic, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picLogo, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'txtPrice
+        '
+        Me.txtPrice.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtPrice.Location = New System.Drawing.Point(237, 341)
+        Me.txtPrice.Name = "txtPrice"
+        Me.txtPrice.ReadOnly = True
+        Me.txtPrice.Size = New System.Drawing.Size(100, 23)
+        Me.txtPrice.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(144, 344)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(40, 17)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Price"
+        '
+        'lstItems
+        '
+        Me.lstItems.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstItems.FormattingEnabled = True
+        Me.lstItems.ItemHeight = 16
+        Me.lstItems.Location = New System.Drawing.Point(18, 168)
+        Me.lstItems.Name = "lstItems"
+        Me.lstItems.Size = New System.Drawing.Size(108, 324)
+        Me.lstItems.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(144, 374)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Quantity"
+        '
+        'txtQuantity
+        '
+        Me.txtQuantity.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtQuantity.Location = New System.Drawing.Point(237, 371)
+        Me.txtQuantity.Name = "txtQuantity"
+        Me.txtQuantity.Size = New System.Drawing.Size(34, 23)
+        Me.txtQuantity.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(144, 409)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(70, 17)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Sub-Total"
+        '
+        'txtSubTotal
+        '
+        Me.txtSubTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSubTotal.Location = New System.Drawing.Point(237, 406)
+        Me.txtSubTotal.Name = "txtSubTotal"
+        Me.txtSubTotal.ReadOnly = True
+        Me.txtSubTotal.Size = New System.Drawing.Size(100, 23)
+        Me.txtSubTotal.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(144, 441)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(31, 17)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Tax"
+        '
+        'txtTax
+        '
+        Me.txtTax.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTax.Location = New System.Drawing.Point(237, 438)
+        Me.txtTax.Name = "txtTax"
+        Me.txtTax.ReadOnly = True
+        Me.txtTax.Size = New System.Drawing.Size(100, 23)
+        Me.txtTax.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(144, 470)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(40, 17)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Total"
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTotal.Location = New System.Drawing.Point(237, 467)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.ReadOnly = True
+        Me.txtTotal.Size = New System.Drawing.Size(100, 23)
+        Me.txtTotal.TabIndex = 10
+        '
+        'btnClearOrder
+        '
+        Me.btnClearOrder.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClearOrder.Location = New System.Drawing.Point(370, 255)
+        Me.btnClearOrder.Name = "btnClearOrder"
+        Me.btnClearOrder.Size = New System.Drawing.Size(118, 36)
+        Me.btnClearOrder.TabIndex = 16
+        Me.btnClearOrder.Text = "&Restart Order"
+        Me.btnClearOrder.UseVisualStyleBackColor = True
+        '
+        'btnCheckOut
+        '
+        Me.btnCheckOut.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCheckOut.Location = New System.Drawing.Point(370, 298)
+        Me.btnCheckOut.Name = "btnCheckOut"
+        Me.btnCheckOut.Size = New System.Drawing.Size(118, 36)
+        Me.btnCheckOut.TabIndex = 17
+        Me.btnCheckOut.Text = "&Check Out"
+        Me.btnCheckOut.UseVisualStyleBackColor = True
+        '
+        'pic
+        '
+        Me.pic.InitialImage = CType(resources.GetObject("pic.InitialImage"), System.Drawing.Image)
+        Me.pic.Location = New System.Drawing.Point(170, 168)
+        Me.pic.Name = "pic"
+        Me.pic.Size = New System.Drawing.Size(190, 140)
+        Me.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.pic.TabIndex = 18
+        Me.pic.TabStop = False
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(370, 340)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(118, 36)
+        Me.btnExit.TabIndex = 19
+        Me.btnExit.Text = "&EXIT"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.Location = New System.Drawing.Point(18, 139)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(41, 17)
+        Me.Label7.TabIndex = 21
+        Me.Label7.Text = "Items"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label8.Location = New System.Drawing.Point(144, 317)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(34, 17)
+        Me.Label8.TabIndex = 23
+        Me.Label8.Text = "Item"
+        '
+        'txtItem
+        '
+        Me.txtItem.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtItem.Location = New System.Drawing.Point(237, 314)
+        Me.txtItem.Name = "txtItem"
+        Me.txtItem.ReadOnly = True
+        Me.txtItem.Size = New System.Drawing.Size(100, 23)
+        Me.txtItem.TabIndex = 22
+        '
+        'btnUpdate
+        '
+        Me.btnUpdate.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnUpdate.Location = New System.Drawing.Point(370, 213)
+        Me.btnUpdate.Name = "btnUpdate"
+        Me.btnUpdate.Size = New System.Drawing.Size(118, 36)
+        Me.btnUpdate.TabIndex = 24
+        Me.btnUpdate.Text = "&Update"
+        Me.btnUpdate.UseVisualStyleBackColor = True
+        '
+        'picLogo
+        '
+        Me.picLogo.Location = New System.Drawing.Point(112, 12)
+        Me.picLogo.Name = "picLogo"
+        Me.picLogo.Size = New System.Drawing.Size(296, 144)
+        Me.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.picLogo.TabIndex = 25
+        Me.picLogo.TabStop = False
+        '
+        'Form1
+        '
+        Me.AcceptButton = Me.btnCheckOut
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(528, 509)
+        Me.Controls.Add(Me.picLogo)
+        Me.Controls.Add(Me.btnUpdate)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.txtItem)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.pic)
+        Me.Controls.Add(Me.btnCheckOut)
+        Me.Controls.Add(Me.btnClearOrder)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtTax)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txtSubTotal)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtQuantity)
+        Me.Controls.Add(Me.lstItems)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtPrice)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Motorcycle Parts Shop"
+        CType(Me.pic, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picLogo, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtPrice As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lstItems As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtQuantity As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtSubTotal As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtTax As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents btnClearOrder As Button
+    Friend WithEvents btnCheckOut As Button
+    Friend WithEvents pic As PictureBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents txtItem As TextBox
+    Friend WithEvents btnUpdate As Button
+    Friend WithEvents picLogo As PictureBox
+End Class
---
+++ Homework_4/Form1.vb
@@ -0,0 +1,134 @@
+﻿
+''James Morand
+''Visual Basic 105 
+''Project_4
+''Professor: Ron Kessller
+''4/25/2018
+
+
+Public Class Form1
+
+    ''global values for math calculations
+
+    Dim quantity = 0
+    Dim price = 0
+    Dim tax = 0
+    Dim taxpercent = 0.08D
+    Dim subtotal = 0
+    Dim total = 0
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        picLogo.Image = Image.FromFile("..\..\pics\logo.png")
+
+        ''create items in listbox with a name, price, and image
+        lstItems.Items.Add("Clutch                        400.00                                 clutch.jpg")
+        lstItems.Items.Add("Spark Plugs                   10.00                                sparkplug.jpg")
+        lstItems.Items.Add("Rear Tire                     150.00                               reartire.jpg")
+        lstItems.Items.Add("Front Tire                    120.00                              FrontTire.jpg")
+        lstItems.Items.Add("Helmet                        650.00                                 helmet.jpg")
+        lstItems.Items.Add("Gloves                        135.00                                  glove.jpg")
+        lstItems.Items.Add("Boots                         420.00                                   boot.jpg")
+        lstItems.Items.Add("Jacket                        350.00                                 jacket.jpg")
+        lstItems.Items.Add("Pants                         440.00                                  pants.jpg")
+        lstItems.Items.Add("Track Suit                   1150.00                                   suit.jpg")
+        lstItems.Items.Add("Fule Additive                   5.00                               fuleadditive.jpg")
+        lstItems.Items.Add("Phone Mount                    75.00                               phonemount.jpg")
+        lstItems.Items.Add("Communicator                  250.00                                 sena20.jpg")
+        lstItems.Items.Add("Helmet Visor                   50.00                                 visors.jpg")
+        lstItems.Items.Add("Key Chain                     150.00                               keychain.jpg")
+
+
+        lstItems.SelectedIndex = 0
+
+
+    End Sub
+
+    Private Sub lstItems_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstItems.SelectedIndexChanged
+
+        txtPrice.Text = ""
+        txtQuantity.Text = 1
+
+        Dim ItemName As String = ""
+        Dim ItemPrice As String = ""
+        Dim Itempicture As String = ""
+
+        ''trim the name, price, and image from the listbox
+        ItemName = Trim(Mid(lstItems.SelectedItem, 1, 16))
+        ItemPrice = Trim(Mid(lstItems.SelectedItem, 16, 25))
+        Itempicture = Trim(Mid(lstItems.SelectedItem, 50))
+
+        ''set textbox as tim values
+        txtItem.Text = ItemName
+        txtPrice.Text = FormatCurrency(Convert.ToDouble(ItemPrice))
+
+        ''display image of item in picture box 
+        pic.Image = Image.FromFile("..\..\pics\" & Itempicture)
+
+        ''calculate subtotal
+        price = CDbl(txtPrice.Text)
+        quantity = CInt(txtQuantity.Text)
+
+        subtotal = FormatCurrency(quantity * price)
+        txtSubTotal.Text = subtotal
+
+    End Sub
+
+    ''update button will recalculate the subtotal with the changed quantity
+    Private Sub btnUpdate_Click(sender As Object, e As EventArgs) Handles btnUpdate.Click
+        ''update subtotal with new quantity
+        price = CDbl(txtPrice.Text)
+        quantity = CDbl(txtQuantity.Text)
+        ''subtotal calculations
+        subtotal = FormatCurrency(quantity * price)
+        txtSubTotal.Text = subtotal
+
+    End Sub
+
+    ''reset and start from the beginning
+    Private Sub btnClearOrder_Click(sender As Object, e As EventArgs) Handles btnClearOrder.Click
+
+        lstItems.ClearSelected()
+        txtTotal.Clear()
+        txtTax.Clear()
+        txtQuantity.Focus()
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ''message box with warning to make sure they want to leave
+        If MessageBox.Show("You sure you want to leave?", "Warning!", MessageBoxButtons.YesNo,
+                           MessageBoxIcon.Warning) = MsgBoxResult.Yes Then
+            ''close the application
+            Me.Close()
+        Else
+            ''do nothihng
+        End If
+    End Sub
+
+    Private Sub btnCheckOut_Click(sender As Object, e As EventArgs) Handles btnCheckOut.Click
+        If MessageBox.Show("Are you ready to check out?", "Shopping Cart", MessageBoxButtons.YesNo,
+                           MessageBoxIcon.Question) = MsgBoxResult.Yes Then
+            price = CDbl(txtPrice.Text)
+            quantity = CDbl(txtQuantity.Text)
+
+            ''calculations
+            tax = taxpercent * subtotal
+            total = tax + price
+            subtotal = FormatCurrency(quantity * price)
+
+            ''display the calculations
+            txtSubTotal.Text = FormatCurrency(subtotal)
+            txtTax.Text = FormatCurrency(tax)
+            txtTotal.Text = FormatCurrency(total)
+
+            ''goodbye message with grand total and close application
+            MessageBox.Show("Your total was " + txtTotal.Text, "Thanks For Shopping", MessageBoxButtons.OK)
+            Me.Close()
+
+        Else
+            ''do nothing and return for shopping
+        End If
+    End Sub
+End Class
